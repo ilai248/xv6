139067e91cf42298632f52bb88b692be223befc3
Ilai Shoshani <ilaish27@gmail.com>
2024-09-25 07:59:45 -0400

    Completed part 1 of traps lab

---
+++ user/code.c
@@ -0,0 +1,10 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+void main(int argc, char* argv[]) {
+    printf("x=%d y=%d", 3);
+    exit(0);
+}
+
