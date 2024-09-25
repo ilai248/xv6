d2e7b3428bca329da580dab6f551c1e4bd66b178
Ilai Shoshani <ilaish27@gmail.com>
2024-09-25 03:06:30 -0400

    Updated to last version

--- kernel/proc.c
+++ kernel/proc.c
@@ -141,15 +141,15 @@ found:
     return 0;
   }
 
-// #ifdef LAB_PGTBL
-//   // Allocate the usyscall page.
-//   if((p->syspage = (struct usyscall*)kalloc()) == 0){
-//     freeproc(p);
-//     release(&p->lock);
-//     return 0;
-//   }
-//   p->syspage->pid = p->pid;
-// #endif
+#ifdef LAB_PGTBL
+  // Allocate the usyscall page.
+  if((p->syspage = (struct usyscall*)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->syspage->pid = p->pid;
+#endif
 
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
@@ -179,10 +179,10 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
 
-// #ifdef LAB_PGTBL
-//   if(p->syspage)
-//     kfree((void*)p->syspage);
-// #endif
+#ifdef LAB_PGTBL
+  if(p->syspage)
+    kfree((void*)p->syspage);
+#endif
 
   p->trapframe = 0;
   if(p->pagetable)
@@ -229,15 +229,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
-// #ifdef LAB_PGTBL
-//   // Map the syscall-optimization page.
-//   if (mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->syspage), PTE_R | PTE_U) < 0) {
-//     uvmunmap(pagetable, TRAPFRAME, 1, 0);
-//     uvmunmap(pagetable, TRAMPOLINE, 1, 0);
-//     uvmfree(pagetable, 0);
-//     return 0;
-//   }
-// #endif
+#ifdef LAB_PGTBL
+  // Map the syscall-optimization page.
+  if (mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->syspage), PTE_R | PTE_U) < 0) {
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+#endif
 
   return pagetable;
 }
@@ -250,9 +250,9 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
 
-// #ifdef LAB_PGTBL
-//   uvmunmap(pagetable, USYSCALL, 1, 0);
-// #endif
+#ifdef LAB_PGTBL
+  uvmunmap(pagetable, USYSCALL, 1, 0);
+#endif
   uvmfree(pagetable, sz);
 }
 
