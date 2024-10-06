f4c2af7af2338225a23bab069f4b221b1ac67d16
Ilai Shoshani <ilaish27@gmail.com>
2024-10-06 14:52:37 -0400

    Push working version with modified timeout

--- kernel/trap.c
+++ kernel/trap.c
@@ -68,6 +68,10 @@ usertrap(void)
   } else if((which_dev = devintr()) != 0){
     // ok
   } else {
+    if (r_scause() == 0xf /* store page fault */) {
+      printf("Page Fault\n");
+    }
+    
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     setkilled(p);
